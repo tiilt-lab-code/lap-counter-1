575841a0bf554687eb26ba5e6791c91cab32c612
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-02-22 10:37:06 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1 && radio.receivedPacket(RadioPacketProperty.SignalStrength) > db_level) {
+        radio.sendValue("serial", radio.receivedPacket(RadioPacketProperty.SerialNumber))
+        total += 1
+        basic.showNumber(total)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    db_level += 5
+    basic.showNumber(db_level)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(db_level)
+})
+input.onButtonPressed(Button.B, function () {
+    db_level += -5
+    basic.showNumber(db_level)
+})
+let db_level = 0
+let total = 0
+radio.setGroup(11)
+total = 0
+db_level = -65
+basic.showIcon(IconNames.SmallDiamond)
+basic.showIcon(IconNames.Diamond)
+basic.forever(function () {
+	
+})
